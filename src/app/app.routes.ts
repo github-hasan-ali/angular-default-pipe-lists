a0198babe6028231407a466204ccfc972d854860
Alper Tayyip <atayyipklc@gmail.com>
2024-01-04 01:48:23 +0300

    CurrencyPipe ve JsonPipe eklendi.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { pipe } from 'rxjs';
+import { JsonPipeComponent } from '../components/json-pipe/json-pipe.component';
+import { CurrencyPipeComponent } from '../components/currency-pipe/currency-pipe.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path:"json-pipe",component:JsonPipeComponent},
+    {path:"currency-pipe",component:CurrencyPipeComponent}
+];
--- src/components/currency-pipe/currency-pipe.component.ts
+++ src/components/currency-pipe/currency-pipe.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/currency-pipe';
+
 @Component({
   selector: 'app-currency-pipe',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './currency-pipe.component.html',
   styleUrl: './currency-pipe.component.scss'
 })
-export class CurrencyPipeComponent {
+export class CurrencyPipeComponent{
+
+ product:Product={
+  
+  price:150
+
+
+ }
+
+
+  constructor() { }
+
+  
+    
+
+
+   
 
 }
